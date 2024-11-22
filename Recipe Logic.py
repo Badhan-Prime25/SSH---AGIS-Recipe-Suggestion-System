027b626919e78c3cd5d7009e6b0d2056b3eb9518
Badhan-Prime25 <badhangaurav025@gmail.com>
2024-11-22 16:04:47 +0000

    Adding the initial methods for  file opening and database data alongside the recipe class and matching ingredients logic

--- Recipe Logic.py
+++ Recipe Logic.py
@@ -1,3 +1,40 @@
+import sqlite3 as lite
+import csv
+import os
+
+# Connects to the recipe Database
+recipesDatabase = lite.connect('foodDatabase.db')
+repCur = recipesDatabase.cursor()
+
+def get_recipes():
+    recipe_sql = """""
+                SELECT 
+                    r.recipe_name,
+                    r.instructions,
+                    ri.ingredients,
+                    r.meal_type,
+                    r.veg_status,
+                    r.prep_time AS duration
+                FROM
+                    Recipe AS r
+                JOIN 
+                    Recipe_Ingredient AS ri ON r.recipe_id = ri.recipe_id
+                JOIN 
+                    Ingredients AS i ON ri.ingredient_id = i.ingredient_id
+                GROUP BY  r.recipe_name,r.instructions,ri.ingredients,r.meal_type,r.veg_status,r.prep_time AS duration
+    """""
+    repCur.execute(recipe_sql)
+    recipes = repCur.fetchall()
+    return recipes
+
+def extract_ingredients():
+    ingredients = []
+    with open('ENTER_CSV_FILE.csv', mode = 'r') as file:
+        csv_file = csv.reader(file, delimiter=',')
+        for row in csv_file:
+            ingredients.extend(row)
+    return ingredients
+
 class Recipe:
     def __init__(self, name, instructions, ingredients, meal_type, vegetarian, prep_time):
         self.name = name
