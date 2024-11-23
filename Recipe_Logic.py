798dbb4f7735d6b8f635a0951482ffd66baf6c4b
Badhan-Prime25 <badhangaurav025@gmail.com>
2024-11-23 16:30:39 +0000

    Amending Recipe_Logic.py so that the recipe class such that has the right amount of data the database can offer, as well as correctly connecting the database to the sql file and creating instances for the class recipes with the database information

---
+++ Recipe_Logic.py
@@ -0,0 +1,71 @@
+import sqlite3 as lite
+import csv
+#import os
+
+# Connects to the recipe Database
+recipesDatabase = lite.connect('foodDatabase.db')
+repCur = recipesDatabase.cursor()
+
+class Recipe:
+    def __init__(self, name, instructions, ingredients, meal_type):
+        self.name = name
+        self.instructions = instructions
+        self.ingredients = ingredients
+        self.meal_type = meal_type
+        #self.vegetarian = vegetarian Database does not have any column with these values
+        #self.prep_time = prep_time
+
+def get_recipes():
+    # SQL statement to get required data
+    recipe_sql = """
+                    SELECT 
+                        r.recipe_name,
+                        r.instructions,
+                        GROUP_CONCAT (i.ingredient_name) AS ingredients,
+                        r.meal_type
+                    FROM
+                        Recipes AS r
+                    JOIN 
+                        Recipe_Ingredient AS ri ON r.recipe_id = ri.recipe_id
+                    JOIN 
+                        Ingredients AS i ON ri.ingredient_id = i.ingredient_id
+                    GROUP BY  r.recipe_name, r.instructions, r.meal_type
+        """
+    repCur.execute(recipe_sql) # executes SQL Statement
+    rows = repCur.fetchall()
+    recipes =[]
+    # Makes an instance for each recipe
+    for row in rows:
+        name,instructions,ingredients_list,meal_type = row
+        ingredients_list = ingredients_list.split(',')
+        recipes.append(Recipe(name, instructions, ingredients_list, meal_type))
+    return recipes # Returns a lists of instances
+
+
+def extract_ingredients():
+    ingredients = []
+    with open('ENTER_CSV_FILE.csv', mode = 'r') as file:
+        csv_file = csv.reader(file, delimiter=',')
+        for row in csv_file:
+            ingredients.extend(row)
+    return ingredients
+
+
+
+
+def match_ingredients(recipes, ingredients):
+    possible_recipes = []
+    partial_recipes = []
+    ingredients_list = ingredients.split(',')
+    for recipe in recipes:
+        no_ingredients = len(recipe.ingredients)
+        matching_ingredients = []
+        for ingredient in ingredients_list:
+            if ingredient in recipe.ingredients:
+                matching_ingredients.append(ingredient)
+        if len(matching_ingredients) == no_ingredients:
+            possible_recipes.append(recipe)
+        elif len(matching_ingredients) >= no_ingredients * 0.8:
+            partial_recipes.append(recipe)
+    print(possible_recipes)
+    print(partial_recipes)
